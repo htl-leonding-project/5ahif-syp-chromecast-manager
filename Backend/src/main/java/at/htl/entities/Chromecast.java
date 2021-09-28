9b703806674801915bce89aa001042451a6d027b
Paatz <patrick.spisak@gmx.at>
2021-09-28 13:53:15 +0200

    Further change in Cld

--- Backend/src/main/java/at/htl/entities/Chromecast.java
+++ Backend/src/main/java/at/htl/entities/Chromecast.java
@@ -2,10 +2,13 @@ package at.htl.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Entity
 public class Chromecast {
     @Id
-    private long id;
-    private Boolean borrowed;
+    private int serialNumber;
+    private String name;
+    private String brand;
+    Date installed;
 }
