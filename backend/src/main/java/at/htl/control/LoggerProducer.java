fdb3dfb8b016b3c8a461bdb1bb3c3761d9fc7850
Paatz <patrick.spisak@gmx.at>
2021-10-02 22:32:27 +0200

    further changes

---
+++ backend/src/main/java/at/htl/control/LoggerProducer.java
@@ -0,0 +1,15 @@
+package at.htl.control;
+
+import org.jboss.logging.Logger;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+public class LoggerProducer {
+
+    @Produces
+    public Logger produceLogger(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getBean().getBeanClass());
+    }
+}
+
