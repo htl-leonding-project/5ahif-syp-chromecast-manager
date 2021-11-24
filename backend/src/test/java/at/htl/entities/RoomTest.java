449f1b4e682f7cee0173ca0b65ec9749cabb103b
Paatz <patrick.spisak@gmx.at>
2021-11-24 21:41:02 +0100

    storyboard added

--- backend/src/test/java/at/htl/entities/RoomTest.java
+++ backend/src/test/java/at/htl/entities/RoomTest.java
@@ -1,10 +1,7 @@
 package at.htl.entities;
 
-import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-@QuarkusTest
 class RoomTest {
 
     @Test
