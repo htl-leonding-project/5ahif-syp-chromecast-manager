8c5703e57512e193d07e105f5f298f653efbc6db
Test <superman.batboy@outlook.com>
2021-12-02 14:17:22 +0100

    first db test(read File) completed

--- backend/src/test/java/at/htl/control/RoomRepositoryTest.java
+++ backend/src/test/java/at/htl/control/RoomRepositoryTest.java
@@ -1,10 +1,16 @@
 package at.htl.control;
 
+import at.htl.entities.Room;
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import javax.inject.Inject;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
 @TestMethodOrder(MethodOrderer.MethodName.class)
@@ -12,4 +18,16 @@ public class RoomRepositoryTest {
 
 
     @Inject RoomRepository roomRepository;
+
+
+
+    @DisplayName("Returns the method 'readFile()' a list with 70 names?")
+    @Test
+    void t200_readFile() {
+        List<Room> rooms = roomRepository.readCSV("rooms.csv");
+        assertThat(rooms).hasSize(62);
+    }
+
+
+
 }
