581234120d7300d3412160dbc09c758e8605dee9
Test <superman.batboy@outlook.com>
2021-12-07 12:46:59 +0100

    more tests added assertj

--- backend/src/test/java/at/htl/control/RoomRepositoryTest.java
+++ backend/src/test/java/at/htl/control/RoomRepositoryTest.java
@@ -4,10 +4,13 @@ import at.htl.entities.Room;
 import io.agroal.api.AgroalDataSource;
 import io.quarkus.test.junit.QuarkusTest;
 import org.assertj.db.type.Table;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.junit.jupiter.api.*;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import java.util.List;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @QuarkusTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
@@ -18,12 +21,38 @@ public class RoomRepositoryTest {
     @Inject
     AgroalDataSource ds;
 
+
+    @ConfigProperty(name = "table.room")
+    String roomTable;
+
+
     @BeforeEach
     @Transactional
     public void onInit(){
         roomRepository = new RoomRepository();
         roomRepository.deleteAll(); //does not work somehow
     }
+
+    @DisplayName("Returns the method 'readFile()' a list with 70 names?")
+    @Test
+    void t200_readFile() {
+        List<Room> rooms = roomRepository.readCSV("rooms.csv");
+        assertThat(rooms).hasSize(62);
+    }
+
+    @DisplayName("Is the table-structure of H_ROOM correct?")
+    @Test
+    void t210_isTableStructureRoomCorrect() {
+
+        Table table = new Table(ds, roomTable);
+        org.assertj.db.api.Assertions.assertThat(table)
+                .hasNumberOfColumns(3)
+                .column(0).hasColumnName("R_ID")
+                .column(1).hasColumnName("R_NAME")
+                .column(2).hasColumnName("R_NUMBER");
+    }
+
+
 
     @Order(001)
     @Test
