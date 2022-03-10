77de3d978b4ec753987eb1b3992ae36ff48fd74d
Paatz <patrick.spisak@gmx.at>
2022-03-10 01:39:01 +0100

    create-installation current state

--- frontend/src/app/model/InstallAt.ts
+++ frontend/src/app/model/InstallAt.ts
@@ -2,16 +2,14 @@ import { Room } from "./room";
 import { User } from "./User";
 import { Device } from "./device";
 
-export class InstallAt{
-    constructor(
-        public id: number,
-        public installDate : Date,
-        public description: string,
-        public user : User,
-        public room : Room,
-        public device : Device 
-    ){
-    }
+export interface InstallAt{
+        id: number,
+        installDate : Date,
+        removeDate?: Date,
+        description: string,
+        user : User,
+        room : Room,
+        device : Device 
 }
 
 export class InstallAtDto{
